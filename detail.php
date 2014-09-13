ad5413aa4be28a2f8b19a1f51919ab114e5d8f2c
productiveguy <poddarrishabh@gmail.com>
2014-09-14 01:44:34 +0530

    sql query to fetch list of collages
    and return it as a json

--- detail.php
+++ detail.php
@@ -2,6 +2,18 @@
 
 $ccode = $_POST["ccode"];
 
+$where_subquery = " FROM college WHERE " + ($city)? "city = '$city' AND " + ($state)? "state = '$state' AND " + ($type1)? "type1= '$type1' AND " + ($hostel)? "hostel = '$hostel' AND " + ($name)? "name = '$name' AND " +($extracurricular)? "extracurricular = '$extracurricular' AND " + ($academicfocus)? "academicfocus = '$academicfocus' AND " + ($citytype)? "citytype = '$citytype' AND " + "ccode IN (SELECT ccode FROM branch WHERE " + ($branchname)? "branchname = '$branchname' AND " + ($programme)? "programme = '$programme' AND " + ($eligibility12)? "eligibility12 < '$eligibility12' AND " + ($eligibilityUG)? "eligibilityUG < '$eligibilityUG' AND " + ($eligibilityEntrance)? "eligibilityEntrance = '$eligibilityEntrance' AND applicationdeadline >= curdate() AND " + ($integrated)? "integrated = '$integrated' AND " + ($duration)? "duration = '$duration' AND " + ($type)? "type2 = '$type2')"; 
 
+$list_query = "SELECT ccode, name, city, state" + $where_subquery;
 
+$rows = array();
+$list = mysql_query($list_query);
+if($list){
+	while($row = mysql_fetch_assoc($list)){
+		array_push($rows,$row);
+	}
+}
+$list = json_encode($rows);
+
+echo $list;
 ?>
